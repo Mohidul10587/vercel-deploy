5a184df9d815f5706cfc4f0770436d421eeb75b9
mohid <mohid10587@gmail.com>
2023-04-30 14:07:55 +0600

    almonst complete

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const port = process.env.PORT || 5000
 app.use(express.json());
 app.use(cors())
 const bcrypt = require('bcrypt');
+const crypto = require('crypto');
 const SECRET_KEY = 'b9682406b6545de642ff8026527300b35ec4d70803b4fe40ce37c9ea292634bcb3829fad2e685531abc6bc15e6243f2e06e46e8d9c9c28a407c8f01af5761378';
 
 const uri = 'mongodb+srv://mohid10587:jUWk8Fjxf3UAqusZ@cluster0.aqkhuox.mongodb.net/?retryWrites=true&w=majority'
@@ -42,13 +43,17 @@ async function run() {
         const usersCollection = client.db('ayshi').collection('users')
 
 
-
         app.post("/addProduct", async (req, res) => {
-            const product = req.body
-            const result = await productsCollection.insertOne(product)
-            console.log(result)
-        });
-
+            try {
+              const product = req.body;
+              const result = await productsCollection.insertOne(product);
+              res.status(200).json({ message: "Product added successfully"});
+            } catch (err) {
+              console.error(err);
+              res.status(500).json({ error: "Server error" });
+            }
+          });
+          
 
         app.get('/getProduct', async (req, res) => {
 
@@ -56,6 +61,46 @@ async function run() {
 
             res.send(result)
         })
+
+
+
+
+
+
+
+        // delete a products
+        app.delete('/products/:productId', async (req, res) => {
+            try {
+                const productId = req.params.productId;
+
+                // Check if the product exists
+                const product = await productsCollection.findOne({ _id:new ObjectId(productId) });
+                if (!product) {
+                    return res.status(404).json({ message: 'Product not found' });
+                }
+
+                // Delete the product
+                await productsCollection.deleteOne({ _id: new ObjectId(productId) });
+
+                res.json({ message: 'Product deleted successfully' });
+            } catch (err) {
+                console.error(err);
+                res.status(500).json({ message: 'Server error' });
+            }
+        });
+
+        app.get('/productByFlavour/:flavour', async (req, res) => {
+            const flavour = req.params.flavour;
+            const result = await productsCollection.find({ flavorName:flavour}).toArray();
+
+            if (result) {
+                // Product found
+                res.status(200).json(result);
+            } else {
+                // Product not found
+                res.status(404).json({ message: `Product with id ${productId} not found` });
+            }
+        });
 
 
         app.get('/productDetails/:productId', async (req, res) => {
@@ -70,6 +115,7 @@ async function run() {
                 res.status(404).json({ message: `Product with id ${productId} not found` });
             }
         });
+
 
 
         app.post("/storeIntoCart", async (req, res) => {
@@ -155,7 +201,7 @@ async function run() {
 
                 const user = await usersCollection.findOne({ email });
 
-                const token = jwt.sign({ id: user._id }, SECRET_KEY);
+                const token = jwt.sign({ id: user._id }, SECRET_KEY, { expiresIn: '1d' });
 
                 res.status(200).json({ token });
             } catch (err) {
@@ -182,7 +228,7 @@ async function run() {
                 }
 
                 // Create and sign JWT token
-                const token = jwt.sign({ userId: user._id }, SECRET_KEY, { expiresIn: '1h' });
+                const token = jwt.sign({ userId: user._id }, SECRET_KEY, { expiresIn: '1d' });
 
                 res.json({ token });
             } catch (err) {
@@ -194,19 +240,67 @@ async function run() {
         // check user or admin 
         app.get('/users/:email', async (req, res) => {
             try {
-              const email = req.params.email;
-              const user = await usersCollection.findOne({ email });
-              if (!user) {
-                return res.status(404).json({ message: 'User not found' });
-              }
-              console.log(user)
-              return res.status(200).json(user);
-            } catch (err) {
-              console.error(err);
-              return res.status(500).json({ message: 'Server error' });
-            }
-          });
+                const email = req.params.email;
+                const user = await usersCollection.findOne({ email });
+                if (!user) {
+                    return res.status(404).json({ message: 'User not found' });
+                }
 
+                return res.status(200).json(user);
+            } catch (err) {
+                console.error(err);
+                return res.status(500).json({ message: 'Server error' });
+            }
+        });
+
+
+
+
+        //  forgot password 
+
+        app.post('/reset-password/:email', async (req, res) => {
+            try {
+                const { email } = req.params;
+                const { newPassword } = req.body
+
+                // Check if user exists
+                const user = await usersCollection.findOne({ email });
+                if (!user) {
+                    return res.status(404).json({ message: 'User not found' });
+                }
+
+                // Generate new password
+
+                const hashedPassword = await bcrypt.hash(newPassword, 10);
+
+                // Update user's password
+                await usersCollection.updateOne({ email }, { $set: { password: hashedPassword } });
+
+                // Send new password to user
+                res.json({ message: `Your new password is: ${newPassword}` });
+            } catch (err) {
+                console.error(err);
+                res.status(500).json({ message: 'Server error' });
+            }
+        });
+
+
+
+
+
+
+        //   get all users
+        app.get('/users', async (req, res) => {
+
+            try {
+                const users = await usersCollection.find({}).toArray();
+                console.log(users)
+                res.json(users);
+            } catch (err) {
+                console.error(err);
+                res.status(500).send('Server error');
+            }
+        });
 
         // Route to retrieve the current user's information
         app.get('/me', authenticateToken, async (req, res) => {
